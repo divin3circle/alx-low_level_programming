fd1d124d03ab6786dd208d7adbc7c477b345bc83
Silas Abel <divinecircle07032002@gmail.com>
2022-12-08 13:50:12 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle\n") 
+#include <stdio.h>
+/* Strating point of the program
+*int main - starting point
+*
+*Return: 0 successful
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}

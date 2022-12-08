7b60193c21dca08604ae6fc3f9dc9346abedee25
Silas Abel <divinecircle07032002@gmail.com>
2022-12-08 14:57:45 -0800

    UNIX simple complexity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main -> program entry point
+*
+*Return: Always 1(successful)
+*/
+int main(void)
+{
+	fprint(stderr, "and that piece of art is useful\" - Dora Korpar," "2015-10-19\n");
+	return (1);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+This repo covers projects i will create during the introductory phase of low level programming using C
+Only vim text editor was used

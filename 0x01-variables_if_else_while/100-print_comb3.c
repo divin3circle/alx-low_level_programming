b7c2f4737af5ecbf096e8ff961ffe412d9f51615
Silas Abel <divinecircle07032002@gmail.com>
2022-12-09 17:54:15 -0800

    comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-/* main -> program entry, displays all combinaions of two digits
-*
+/**
+*main -> program entry, displays all combinaions of two digits
 * Return: always 0(success)
 */
 int main(void)

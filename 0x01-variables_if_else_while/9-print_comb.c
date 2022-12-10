c285e0b1dcb2b88c861a581bae92638c940849b8
Silas Abel <divinecircle07032002@gmail.com>
2022-12-09 16:57:11 -0800

    patience...

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,17 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <=9; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		putchar((n % 10) + '0');
-		putchar(',');
+		if (n != 9)
+		{
+			putchar(',');
+		}
+		else
+		{
+			break;
+		}
 		putchar(' ');
 	}
 	putchar('\n');

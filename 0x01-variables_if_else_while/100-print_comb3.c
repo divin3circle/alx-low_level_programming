811a1d4a89f32108cc2a14d0c825e7df4e84d9c8
Silas Abel <divinecircle07032002@gmail.com>
2022-12-09 17:48:17 -0800

    comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* main -> program entry, displays all combinaions of
+* numbers that can be made by two digits
+*
+* Return: always 0(success)
+*/
+int main(void)
+{
+	int i;
+	int x;
+	int z;
+	z = x-i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (x = 0; x<= 9; x++)
+		{
+			if (x == i && (z == 1 || -1))
+				continue;
+			putchar((i % 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}

62d262a7c004ff361b45e90e315bc42f71bb8853
Silas Abel <divinecircle07032002@gmail.com>
2022-12-13 02:47:27 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main -> program entry, prints _putchar in a new line
+*
+* Return: 0 always successful
+*/
+
+int main()
+{
+
+	char mess[] = "_putchar";
+	int i;
+
+/* loop through the message char*/
+	for (i = 0; i < 9; i++)
+	{
+		if (mess[i] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		else
+			putchar(mess[i]);
+	}
+		return 0;
+}
+

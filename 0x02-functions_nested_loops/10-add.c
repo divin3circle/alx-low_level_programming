895e585d462fe25063fd3c2bb0ff2e6a21052dea
Silas Abel <divinecircle07032002@gmail.com>
2022-12-14 07:53:06 -0800

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add - computes the sum of two int
+* @n: one of the int to be computed
+* @m: the other int to be added to n
+*
+* Return: the sum of n and m
+*/
+int add(int n, m)
+{
+	return (n + m);
+}

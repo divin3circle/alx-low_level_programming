bd51feeb33ff24c52fce7fc34330d3c7b9e3dd50
Silas Abel <divinecircle07032002@gmail.com>
2022-12-14 10:50:18 -0800

    101 fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - pogram entry
+*
+* Return: always 0(success)
+*/
+int main(void)
+{
+	int i, n = 50;
+	int t1 = 1, t2 = 2;
+	int next = t1 = t2;
+	printf("%d, %d, ", t1, t2);
+
+	for (i = 3; i <= n; ++i)
+	{
+		printf("%d, ", next);
+		t1 = t2;
+		t2 = next;
+		next = t1 + t2;
+	}
+
+	return (0);
+}

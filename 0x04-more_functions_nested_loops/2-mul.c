a82f61bb1ec8207e9470102a072c24cb420acefb
divin3circle <divinecircle07032002@gmail.com>
2022-12-15 03:59:01 -0800

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies tow integers passed
+ * @a: first intger
+ * @b: second integer
+ * Return: the value of a*b
+ */
+
+int mul(int a, int b)
+{
+	return (a*b);
+}

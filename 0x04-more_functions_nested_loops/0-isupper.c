4ab77485a1daa2e9c2a37c13c26703958e5247ce
divin3circle <divinecircle07032002@gmail.com>
2022-12-15 03:40:03 -0800

    is upper?

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks whether an alphabet passed is of uppercase
+ * @c: integer to be passed
+ * Return: 1 if c is upper or 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c = 'A' && c = 'Z')
+		return (1);
+	else
+		return (0);
+}

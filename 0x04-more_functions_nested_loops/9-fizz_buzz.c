a656a2494b567d3484d42075649d34fdf6029f1f
divin3circle <divinecircle07032002@gmail.com>
2022-12-15 06:01:52 -0800

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,16 +8,22 @@
 
 int main(void)
 {
-	int a, b;
+	int p = 100;
+	int a;
 
-	for (a = 1; a <= 100; a++)
+	a = 1;
+
+	while (a <= p)
 	{
-		if (a % 3 == 0)
+		if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz");
+		else if (a % 3 == 0)
 			printf("Fizz ");
 		else if (a % 5 == 0)
 			printf("Buzz ");
 		else
 			printf("%d ", a);
+		a++;
 	}
 	printf("\n");
 	return (0);

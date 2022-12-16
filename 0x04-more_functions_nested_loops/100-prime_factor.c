ac3ebaec83d73a6451e3a0a9b18b6b9eae3d6e9b
divin3circle <divinecircle07032002@gmail.com>
2022-12-16 03:17:18 -0800

    prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<math.h>
+
+/**
+ * getMaxPrimeFactor - gets the maximu prime factor
+ * @n: number whoose largest prime number is to be found
+ * Return: max, the maximum prime factor
+ */
+int getMaxPrimeFactor(int n) {
+   int i, max = 612852475143;
+
+   while(n % 2 == 0) {
+      max = 2;
+      n = n/2;
+   }
+   for(i = 3; i <= sqrt(n); i=i+2){ 
+      while(n % i == 0) {
+         max = i;
+         n = n/i;
+      }
+   }
+   if(n > 2) {
+      max = n;
+   }
+   return max;
+}
+
+/**
+ * main - program entry point
+ *
+ * Return: 0 always
+ */
+int main(void) 
+{
+   int n = 612852475143;
+
+   printf("%d",getMaxPrimeFactor(n));
+   return(0);
+}

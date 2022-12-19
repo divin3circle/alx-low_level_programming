78a1f066fd47f141406fd32bd0191758fea12994
divin3circle <divinecircle07032002@gmail.com>
2022-12-19 15:35:29 -0800

    put to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts - prints a string plus a new line in stdout
+* @str: adress of string to be printed
+* Return: void
+*/
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++
+	}
+	_putchar('\n');
+}

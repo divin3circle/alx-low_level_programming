936eb5d7a993eff25577307b834f6e0bd8df7240
divin3circle <divinecircle07032002@gmail.com>
2022-12-19 14:40:34 -0800

    swap integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* swap_int - swaps the values of int passed
+* @a: adress of int one
+* @b: adress of second integer
+* Return: void always
+*/
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+}

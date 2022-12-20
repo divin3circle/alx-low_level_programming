47caec6fa0ac720493a359ea7c43dfba022afd90
divin3circle <divinecircle07032002@gmail.com>
2022-12-19 16:05:23 -0800

    every character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* puts2 - prints every char of a string
+* @str: adress of the string
+* Return: void
+*/
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+}

c5e8738d940e66a54a62b7b0e8689f92f5c01814
divin3circle <divinecircle07032002@gmail.com>
2022-12-21 09:30:16 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,16 @@ char *_strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;
 
-	while (dest[index++])
+	while (*(dest + index) != '\0')
+		index++;
+
+	while (dest_len >= 0)
+	{
+		*(dest + index) = *(src + dest_len);
+		if (*(src + dest_len) == '\0')
+			break;
+		index++;
 		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	}
 	return (dest);
 }

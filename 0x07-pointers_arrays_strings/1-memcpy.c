390ad0d564d4afa0469567273f9be295ab10c9a4
divin3circle <divinecircle07032002@gmail.com>
2023-01-04 00:16:04 -0800

    memory copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,14 +3,14 @@
 /**
 * _memcpy - copies a memory area
 * @dest: pointer of the location to be pasted
-* @src: pointer to source 
+* @src: pointer to source
 * @n: number of bytes to be copied
 * Return: pointer to the destnination
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	
+
 	for (i = 0; i < n; i++)
 		*(dest + i) = *(src + i);
 	return (dest);

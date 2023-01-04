3fb6f87ce714f98241e80bf36f80af561f3586a3
divin3circle <divinecircle07032002@gmail.com>
2023-01-04 00:43:21 -0800

    charcter in strng

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strchr - locates a charcter in a string
+* @s: pointer to the string
+* @c: charcter to be located
+* Return: pointer to the first occurence of c
+* or NULL if the charcter is missing
+*/
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
+}

7f8c32bd962d9603bbd4d4bbfe15557d1b686f03
divin3circle <divinecircle07032002@gmail.com>
2023-01-05 10:57:54 -0800

    squreroot

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,7 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int power_operation(int n, int c);
 int is_prime(unsigned int n, unsigned int c);
 int is_prime_number(int n);
 int is_palindrome(char *s);

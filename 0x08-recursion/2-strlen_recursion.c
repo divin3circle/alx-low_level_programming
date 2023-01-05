ed6da7d58a2c5251b20260d204f513c9d4443c62
divin3circle <divinecircle07032002@gmail.com>
2023-01-05 09:17:42 -0800

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - returns string length
+* @s: pointer to the string whoose length
+* is to be determined
+* Return: length of the string
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}

41faf30799de3b4a2d96a96650200ec91fbedbfe
divin3circle <divinecircle07032002@gmail.com>
2023-02-08 09:37:15 -0800

    more linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -13,7 +13,7 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}listint_t;
+} listint_t;
 
 /**
  * struct listp_s - singly linked list
@@ -24,7 +24,7 @@ typedef struct listp_s
 {
 	void *p;
 	struct listp_s *next;
-}listp_t;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
